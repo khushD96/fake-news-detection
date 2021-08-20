5b854ba78c4130622f26ec8be4207b94f31ec8a7
Khush Dwivedi <khushdwivedi@gmail.com>
2021-08-19 20:48:38 -0400

    latest updates

--- Fake_News_Det.py
+++ Fake_News_Det.py
@@ -36,4 +36,4 @@ def predict():
         return render_template('index.html', prediction="Something went wrong")
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True)
